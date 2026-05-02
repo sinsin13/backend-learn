5121ad243d467bf25d1cf9870730e41af0d39ec4
sinsin13 <adsnh13@gmail.com>
2026-05-02 12:59:15 +0530

    added frontend

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is ready!");
+});
+
+// get a list of 5 jokes with id, title, and punchline
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "Why did the scarecrow win an award?",
+      punchline: "Because he was outstanding in his field!",
+    },
+    {
+      id: 2,
+      title: "Why don't scientists trust atoms?",
+      punchline: "Because they make up everything!",
+    },
+    {
+      id: 3,
+      title: "Why did the bicycle fall over?",
+      punchline: "Because it was two-tired!",
+    },
+
+    {
+      id: 4,
+      title: "Why did the math book look sad?",
+      punchline: "Because it had too many problems!",
+    },
+    {
+      id: 5,
+      title: "Why did the chicken join a band?",
+      punchline: "Because it had the drumsticks!",
+    },
+  ];
+  res.json(jokes);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- test1/index.js
+++ test1/index.js
@@ -1,6 +1,7 @@
 
 
-const express = require('express')
+const express = require('express');
+
 const app = express()
 const port = 4000
 
