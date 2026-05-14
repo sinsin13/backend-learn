94e687148d45f44e96dfd49d27069d09a9112d7e
sinsin13 <adsnh13@gmail.com>
2026-05-15 04:21:19 +0530

    day3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        console.log("Data received:", response.data); // ← add this to debug
+        const data = Array.isArray(response.data) ? response.data : [];
+        setJokes(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("/api/jokes")
+  //     .then((response) => {
+  //       setJokes(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
+  // }, []);
 
   return (
     <>
       <h1>Sinha full stack</h1>
+      <p>JOKES : {jokes.length} </p>
+
+      {
+        jokes.map((joke) => (
+          <div key={joke.id}>
+            <h2>{joke.title}</h2>
+            <p>{joke.punchline}</p>
+          </div>
+        ))    
+      }
+
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
